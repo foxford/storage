aa9cac02b26fc5a7f50e1e4b787970338f3e267a
Andrei Nesterov <ae.nesterov@gmail.com>
2018-09-01 16:39:20 +0300

    Use Tower Web

--- src/tool/s3.rs
+++ src/tool/s3.rs
@@ -4,34 +4,34 @@ use rusoto_core::Region;
 use std::time::Duration;
 
 #[derive(Debug)]
-pub struct Options {
+pub(crate) struct Client {
     credentials: AwsCredentials,
     region: Region,
     expires_in: Duration,
 }
 
-impl Options {
-    pub fn new(
+impl Client {
+    pub(crate) fn new(
         key: &str,
         secret: &str,
         region: &str,
         endpoint: &str,
         expires_in: Duration,
-    ) -> Options {
+    ) -> Client {
         let region = Region::Custom {
             name: region.to_string(),
             endpoint: endpoint.to_string(),
         };
         let credentials = AwsCredentials::new(key, secret, None, None);
 
-        Options {
+        Self {
             credentials,
             region,
             expires_in,
         }
     }
 
-    pub fn presigned_url(self: &Options, method: &str, bucket: &str, key: &str) -> String {
+    pub(crate) fn presigned_url(self: &Client, method: &str, bucket: &str, key: &str) -> String {
         let uri = format!("/{bucket}/{key}", bucket = bucket, key = key);
         let mut req = SignedRequest::new(method, "s3", &self.region, &uri);
         req.generate_presigned_url(&self.credentials, &self.expires_in)
