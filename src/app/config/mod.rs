cee24a1c705b4d2230ec7b88f60ed44aad245419
Andrei Nesterov <ae.nesterov@gmail.com>
2018-09-02 16:06:01 +0300

    Allow config substitution using environment variables

--- src/app/config/mod.rs
+++ src/app/config/mod.rs
@@ -22,5 +22,6 @@ pub struct Config {
 pub(crate) fn load() -> Result<Config, config::ConfigError> {
     let mut parser = config::Config::default();
     parser.merge(config::File::with_name("App"))?;
+    parser.merge(config::Environment::with_prefix("APP").separator("__"))?;
     parser.try_into::<Config>()
 }
