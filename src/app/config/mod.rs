e84cc1f4b96fde7d0f1947c76a9e4df6f28ec11a
Andrei Nesterov <ae.nesterov@gmail.com>
2018-09-26 20:18:38 +0300

    Add support for http and trusted types of Authz

--- src/app/config/mod.rs
+++ src/app/config/mod.rs
@@ -1,3 +1,4 @@
+use app::authz;
 use config;
 use jose::Algorithm;
 use std::collections::HashMap;
@@ -7,44 +8,55 @@ use tower_web::middleware::cors::AllowedOrigins;
 mod parse;
 
 #[derive(Debug, Deserialize)]
-pub struct Authn {
-    pub audience: String,
+pub(crate) struct Authn {
+    pub(crate) audience: String,
     #[serde(deserialize_with = "parse::algorithm")]
-    pub algorithm: Algorithm,
+    pub(crate) algorithm: Algorithm,
     #[serde(deserialize_with = "parse::file")]
-    pub key: Vec<u8>,
+    pub(crate) key: Vec<u8>,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Authz {
-    pub uri: Option<String>,
-    pub token: Option<String>,
+#[serde(tag = "type")]
+#[serde(rename_all = "lowercase")]
+pub(crate) enum Authz {
+    Trusted(authz::TrustedClient),
+    Http(authz::HttpClient),
+}
+
+impl Authz {
+    pub(crate) fn client(config: &Authz) -> Box<&authz::Authorization> {
+        match config {
+            Authz::Trusted(inner) => Box::new(inner),
+            Authz::Http(inner) => Box::new(inner),
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Namespaces {
+pub(crate) struct Namespaces {
     pub app: String,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Cors {
+pub(crate) struct Cors {
     #[serde(deserialize_with = "parse::allowed_origins")]
     #[serde(default)]
-    pub allow_origins: AllowedOrigins,
+    pub(crate) allow_origins: AllowedOrigins,
     #[serde(deserialize_with = "parse::duration")]
     #[serde(default)]
-    pub max_age: Duration,
+    pub(crate) max_age: Duration,
 }
 
-pub type AuthnMap = HashMap<String, Authn>;
-pub type AuthzMap = HashMap<String, Authz>;
+pub(crate) type AuthnMap = HashMap<String, Authn>;
+pub(crate) type AuthzMap = HashMap<String, Authz>;
 
 #[derive(Debug, Deserialize)]
-pub struct Config {
-    pub authn: AuthnMap,
-    pub authz: AuthzMap,
-    pub namespaces: Namespaces,
-    pub cors: Cors,
+pub(crate) struct Config {
+    pub(crate) authn: AuthnMap,
+    pub(crate) authz: AuthzMap,
+    pub(crate) namespaces: Namespaces,
+    pub(crate) cors: Cors,
 }
 
 pub(crate) fn load() -> Result<Config, config::ConfigError> {
