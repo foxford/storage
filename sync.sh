72acc3beab2baf9090be57279c788adf38abe345
Олег Бобок <bobizma@foxford.ru>
2018-11-09 19:59:24 +0300

    Fix naming

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-MEDIA_EDITOR_WEB_BUCKET = 'docs.netology-group.services'
+DOC_WEB_BUCKET = 'docs.netology-group.services'
 
-lftp -c "open -u ${SFTP_USER},${SFTP_PASSWORD} sftp.selcdn.ru; mirror --parallel=4 --no-empty-dirs --no-perms --exclude-glob .DS_Store --reverse --verbose -e docs/book ${MEDIA_EDITOR_WEB_BUCKET}/storage"
+lftp -c "open -u ${SFTP_USER},${SFTP_PASSWORD} sftp.selcdn.ru; mirror --parallel=4 --no-empty-dirs --no-perms --exclude-glob .DS_Store --reverse --verbose -e docs/book ${DOC_WEB_BUCKET}/storage"
