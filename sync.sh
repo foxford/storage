af48097c696d6cc1870b360f19ad08740809b36c
Олег Бобок <bobizma@foxford.ru>
2018-11-09 18:03:24 +0300

    Fix build dir name

--- sync.sh
+++ sync.sh
@@ -2,4 +2,4 @@
 
 MEDIA_EDITOR_WEB_BUCKET = 'docs.netology-group.services'
 
-lftp -c "open -u ${SFTP_USER},${SFTP_PASSWORD} sftp.selcdn.ru; mirror --parallel=4 --no-empty-dirs --no-perms --exclude-glob .DS_Store --reverse --verbose -e docs/html ${MEDIA_EDITOR_WEB_BUCKET}/storage"
+lftp -c "open -u ${SFTP_USER},${SFTP_PASSWORD} sftp.selcdn.ru; mirror --parallel=4 --no-empty-dirs --no-perms --exclude-glob .DS_Store --reverse --verbose -e docs/book ${MEDIA_EDITOR_WEB_BUCKET}/storage"
