73e350a32727869529f0944193938aa8a36469a8
Олег Бобок <bobizma@foxford.ru>
2018-11-13 19:38:25 +0300

    Fix sync

--- sync.sh
+++ sync.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-DOC_WEB_BUCKET = 'docs.netology-group.services'
-
+export DOC_WEB_BUCKET='docs.netology-group.services'
 lftp -c "open -u ${SFTP_USER},${SFTP_PASSWORD} sftp.selcdn.ru; mirror --parallel=4 --no-empty-dirs --no-perms --exclude-glob .DS_Store --reverse --verbose -e docs/book ${DOC_WEB_BUCKET}/storage"
