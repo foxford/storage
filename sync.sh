af2280baa7c207b65c3c724c539e715382f27fa0
Andrei Nesterov <ae.nesterov@gmail.com>
2018-11-23 14:07:20 -0800

    Update docs stage

--- sync.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-export DOC_WEB_BUCKET='docs-netology-group.services'
-lftp -c "open -u ${SFTP_USER},${SFTP_PASSWORD} sftp.selcdn.ru; mirror --parallel=4 --no-empty-dirs --no-perms --exclude-glob .DS_Store --reverse --verbose -e docs/book ${DOC_WEB_BUCKET}/storage"
