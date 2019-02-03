844912518236521437c0bf37c9452df6d7c32b03
Andrei Nesterov <ae.nesterov@gmail.com>
2019-02-02 17:04:41 -0800

    Split authn module into token and extract

--- src/authn/jose/extract.rs
+++ src/authn/jose/extract.rs
@@ -1,9 +1,11 @@
+use crate::authn::jose::token;
 use crate::authn::AccountId;
 use http::header::HeaderValue;
 use http::StatusCode;
-use jsonwebtoken::{decode, Algorithm, TokenData, Validation};
-use serde_derive::{Deserialize, Serialize};
-use std::collections::HashMap;
+use jsonwebtoken::{Algorithm, Validation};
+use serde_derive::Deserialize;
+use std::collections::{HashMap, HashSet};
+use std::ops::Deref;
 use tower_web::extract::{Context, Error as ExtractError, Extract, Immediate};
 use tower_web::util::BufStream;
 use tower_web::{Error, ErrorBuilder};
@@ -14,7 +16,7 @@ pub(crate) type ConfigMap = HashMap<String, Config>;
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct Config {
-    audience: String,
+    audience: HashSet<String>,
     #[serde(deserialize_with = "crate::serde::algorithm")]
     algorithm: Algorithm,
     #[serde(deserialize_with = "crate::serde::file")]
@@ -23,12 +25,39 @@ pub(crate) struct Config {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct Claims {
-    iss: String,
-    aud: String,
-    sub: String,
-    exp: Option<u64>,
+#[derive(Debug)]
+pub(crate) struct BearerToken {
+    inner: String,
+}
+
+impl Deref for BearerToken {
+    type Target = str;
+
+    fn deref(&self) -> &str {
+        &self.inner
+    }
+}
+
+impl BearerToken {
+    fn new(inner: &str) -> Self {
+        Self {
+            inner: inner.to_owned(),
+        }
+    }
+}
+
+impl<B: BufStream> Extract<B> for BearerToken {
+    type Future = Immediate<BearerToken>;
+
+    fn extract(context: &Context) -> Self::Future {
+        match context.request().headers().get(http::header::AUTHORIZATION) {
+            Some(header) => match parse_bearer_token(&header) {
+                Ok(token) => Immediate::ok(BearerToken::new(token)),
+                Err(err) => Immediate::err(err),
+            },
+            None => Immediate::err(missing_token_error().into()),
+        }
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -54,20 +83,33 @@ impl<B: BufStream> Extract<B> for AccountId {
 
 fn extract_account_id(header: &HeaderValue, authn: &ConfigMap) -> Result<AccountId, ExtractError> {
     let token = parse_bearer_token(header)?;
-    let parts = parse_jws_compact(token)?;
-    let config = authn.get(&parts.claims.iss).ok_or_else(|| {
+    let parts = token::parse_jws_compact(token)
+        .map_err(|err| ExtractError::from(error().detail(&err.to_string()).build()))?;
+    let config = authn.get(parts.claims.issuer()).ok_or_else(|| {
         let detail = format!(
             "issuer = {} of the authentication token is not allowed",
-            &parts.claims.iss,
+            parts.claims.issuer(),
         );
         ExtractError::from(error().detail(&detail).build())
     })?;
 
-    let mut verifier = Validation::new(config.algorithm);
-    verifier.set_audience(&config.audience);
-    verifier.validate_exp = parts.claims.exp.is_some();
+    // NOTE: we consider the token valid if its audience matches at least
+    // one audience from the app config for the same issuer.
+    // We can't use 'verifier.set_audience(&config.audience)' because it's
+    // succeed if only all values from the app config represented in the token.
+    if !config.audience.contains(parts.claims.audience()) {
+        let detail = format!(
+            "audience = {} of the authentication token is not allowed",
+            parts.claims.audience(),
+        );
+        return Err(ExtractError::from(error().detail(&detail).build()));
+    }
 
-    decode_account_id(token, &verifier, config.key.as_ref())
+    let mut verifier = Validation::new(config.algorithm);
+    verifier.validate_exp = parts.claims.expiration_time().is_some();
+
+    token::decode_account_id(token, &verifier, config.key.as_ref())
+        .map_err(|err| ExtractError::from(error().detail(&err.to_string()).build()))
 }
 
 fn parse_bearer_token(header: &HeaderValue) -> Result<&str, ExtractError> {
@@ -90,32 +132,6 @@ fn parse_bearer_token(header: &HeaderValue) -> Result<&str, ExtractError> {
             .build()
             .into()),
     }
-}
-
-fn parse_jws_compact(token: &str) -> Result<TokenData<Claims>, ExtractError> {
-    jsonwebtoken::dangerous_unsafe_decode(token).map_err(|_| {
-        ExtractError::from(
-            error()
-                .detail("invalid claims of the authentication token")
-                .build(),
-        )
-    })
-}
-
-fn decode_account_id(
-    token: &str,
-    verifier: &Validation,
-    key: &[u8],
-) -> Result<AccountId, ExtractError> {
-    let data = decode::<Claims>(token, key, &verifier).map_err(|_| {
-        ExtractError::from(
-            error()
-                .detail("verification of the authentication token failed")
-                .build(),
-        )
-    })?;
-
-    Ok(AccountId::new(&data.claims.sub, &data.claims.aud))
 }
 
 ////////////////////////////////////////////////////////////////////////////////
