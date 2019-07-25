0a4519308d81ba95377fbbf53d8771a96a4d0f79
Andrei Nesterov <ae.nesterov@gmail.com>
2019-07-25 12:25:23 -0700

    Add support for the cache

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate tower_web;
 
+use svc_authz::cache::{create_pool, Cache};
+
 fn main() {
     env_logger::init();
 
@@ -18,7 +20,30 @@ fn main() {
         ::std::time::Duration::from_secs(300),
     );
 
-    app::run(s3);
+    let cache = var("CACHE_URL").ok().map(|url| {
+        let size = var("CACHE_POOL_SIZE")
+            .map(|val| {
+                val.parse::<u32>()
+                    .expect("Error converting CACHE_POOL_SIZE variable into u32")
+            })
+            .unwrap_or_else(|_| 5);
+        let timeout = var("CACHE_POOL_TIMEOUT")
+            .map(|val| {
+                val.parse::<u64>()
+                    .expect("Error converting CACHE_POOL_TIMEOUT variable into u64")
+            })
+            .unwrap_or_else(|_| 5);
+        let expiration_time = var("CACHE_EXPIRATION_TIME")
+            .map(|val| {
+                val.parse::<u64>()
+                    .expect("Error converting CACHE_EXPIRATION_TIME variable into u64")
+            })
+            .unwrap_or_else(|_| 300);
+
+        Cache::new(create_pool(&url, size, timeout), expiration_time)
+    });
+
+    app::run(s3, cache);
 }
 
 mod app;
