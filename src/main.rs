410bf1167fb8c2fb7e3f23c4deda0e7851db75f8
Andrei Nesterov <ae.nesterov@gmail.com>
2020-05-16 00:38:47 +0300

    Add support for CACHE_POOL_IDLE_SIZE

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate diesel;
 #[macro_use]
 extern crate tower_web;
 
-use svc_authz::cache::{create_pool, Cache};
+use svc_authz::cache::{create_pool2, Cache};
 
 fn main() {
     env_logger::init();
@@ -31,28 +31,44 @@ fn main() {
         })
         .ok();
 
-    let cache = var("CACHE_URL").ok().map(|url| {
-        let size = var("CACHE_POOL_SIZE")
-            .map(|val| {
-                val.parse::<u32>()
-                    .expect("Error converting CACHE_POOL_SIZE variable into u32")
-            })
-            .unwrap_or_else(|_| 5);
-        let timeout = var("CACHE_POOL_TIMEOUT")
-            .map(|val| {
-                val.parse::<u64>()
-                    .expect("Error converting CACHE_POOL_TIMEOUT variable into u64")
-            })
-            .unwrap_or_else(|_| 5);
-        let expiration_time = var("CACHE_EXPIRATION_TIME")
-            .map(|val| {
-                val.parse::<u64>()
-                    .expect("Error converting CACHE_EXPIRATION_TIME variable into u64")
-            })
-            .unwrap_or_else(|_| 300);
+    let cache = var("CACHE_ENABLED")
+        .ok()
+        .and_then(|val| match val.as_ref() {
+            "1" => {
+                let url = var("CACHE_URL").unwrap_or_else(|_| panic!("Missing CACHE_URL variable"));
 
-        Cache::new(create_pool(&url, size, timeout), expiration_time)
-    });
+                let size = var("CACHE_POOL_SIZE")
+                    .map(|val| {
+                        val.parse::<u32>()
+                            .expect("Error converting CACHE_POOL_SIZE variable into u32")
+                    })
+                    .unwrap_or_else(|_| 5);
+                let idle_size = var("CACHE_POOL_IDLE_SIZE")
+                    .map(|val| {
+                        val.parse::<u32>()
+                            .expect("Error converting CACHE_POOL_IDLE_SIZE variable into u32")
+                    })
+                    .ok();
+                let timeout = var("CACHE_POOL_TIMEOUT")
+                    .map(|val| {
+                        val.parse::<u64>()
+                            .expect("Error converting CACHE_POOL_TIMEOUT variable into u64")
+                    })
+                    .unwrap_or_else(|_| 5);
+                let expiration_time = var("CACHE_EXPIRATION_TIME")
+                    .map(|val| {
+                        val.parse::<u64>()
+                            .expect("Error converting CACHE_EXPIRATION_TIME variable into u64")
+                    })
+                    .unwrap_or_else(|_| 300);
+
+                Some(Cache::new(
+                    create_pool2(&url, size, idle_size, timeout),
+                    expiration_time,
+                ))
+            }
+            _ => None,
+        });
 
     app::run(db, cache);
 }
