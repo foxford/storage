ea2d80b75fdfe0445cb4561fc56b14cbf457a065
Максим Золотухин <m.v.zolotukhin@foxford.ru>
2023-06-13 15:54:18 +0300

    more logs

--- src/app/endpoints/setstate.rs
+++ src/app/endpoints/setstate.rs
@@ -5,8 +5,9 @@ use axum::{
 use http::{header::REFERER, Response, StatusCode};
 use std::sync::Arc;
 
+use super::super::extractor::AccountIdExtractor;
 use svc_authn::AccountId;
-use svc_utils::extractors::AccountIdExtractor;
+//use svc_utils::extractors::AccountIdExtractor;
 
 use super::{s3_object, valid_referer, wrap_error};
 use crate::app::{authz::AuthzObject, context::AppContext};
